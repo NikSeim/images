d42f96a112a6a27260e14041d0fd31c9becd7e1b
NikSeim <patosinnikita@gmail.com>
2024-08-29 14:38:02 +0300

    Delete js directory

--- js/stavki.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const backButton = document.getElementById('back-button');
-    if (backButton) {
-        backButton.addEventListener('click', function() {
-            localStorage.setItem('removeTrader', 'true'); // Установим флаг для удаления торговца
-            window.location.href = '/game_attemp0_2-main123/index.html'; // Возвращаемся на index.html
-        });
-    }
-});
